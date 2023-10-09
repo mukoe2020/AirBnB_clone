a9ca2d9f9452aed1a1554eb058db3e8a8d516372
habibaadam <habiepalmer@gmail.com>
2023-10-09 17:03:47 +0000

    trying out with codes

--- testing_cmd.py
+++ testing_cmd.py
@@ -1,3 +1,22 @@
-wfewfw
-adding more
-using terminal for merging
+#!/usr/bin/python3
+
+import cmd
+
+class MyCLI(cmd.Cmd):
+    def do_hello(self, person):
+        """Greeting a person"""
+        if person:
+            print("hello there!", person)
+        else:
+            print("hi you!")
+
+    def do_EOF(self, arg):
+        """Exiting the program with ctrl D"""
+        return True
+    
+    def do_exit(self, arg):
+        """Exiting the console with exit or ctrl C"""
+        return True
+    
+if __name__ == "__main__":
+    MyCLI().cmdloop()
