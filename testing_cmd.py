6f577f2f2dc1369021aeaf82b5ce43c191ab0abf
habibaadam <habiepalmer@gmail.com>
2023-10-09 17:09:54 +0000

    merging

--- testing_cmd.py
+++ testing_cmd.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
 import cmd
-<<<<<<< HEAD
-=======
 from datetime import datetime
->>>>>>> habies_branch
 
 class MyCLI(cmd.Cmd):
     def do_hello(self, person):
@@ -14,14 +11,11 @@ class MyCLI(cmd.Cmd):
         else:
             print("hi you!")
 
-<<<<<<< HEAD
-=======
     def do_time(self, arg):
         """Displaying the current time when requested"""
         this_moment = datetime.now()
         print("The current time now is: ", this_moment)
 
->>>>>>> habies_branch
     def do_EOF(self, arg):
         """Exiting the program with ctrl D"""
         return True
