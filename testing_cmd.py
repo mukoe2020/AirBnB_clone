4082c14c7f1e00058d1bcb39e12d0c2d5f3d378e
habibaadam <habiepalmer@gmail.com>
2023-10-09 17:07:01 +0000

    updated a command

--- testing_cmd.py
+++ testing_cmd.py
@@ -1,3 +1,28 @@
-wfewfw
-adding more
-using terminal for merging
+#!/usr/bin/python3
+
+import cmd
+from datetime import datetime
+
+class MyCLI(cmd.Cmd):
+    def do_hello(self, person):
+        """Greeting a person"""
+        if person:
+            print("hello there!", person)
+        else:
+            print("hi you!")
+
+    def do_time(self, arg):
+        """Displaying the current time when requested"""
+        this_moment = datetime.now()
+        print("The current time now is: ", this_moment)
+
+    def do_EOF(self, arg):
+        """Exiting the program with ctrl D"""
+        return True
+    
+    def do_exit(self, arg):
+        """Exiting the console with exit or ctrl C"""
+        return True
+    
+if __name__ == "__main__":
+    MyCLI().cmdloop()
