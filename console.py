6768010bdbf6054c8ce911bbd280b830eb19466b
habibaadam <habiepalmer@gmail.com>
2023-10-10 14:33:53 +0000

    in progress

--- console.py
+++ console.py
@@ -2,6 +2,13 @@
 
 import cmd
 import models
+from models.base_model import BaseModel
+
+""" Create a dictionary mapping class names to class objects """
+class_names = {
+    'BaseModel': BaseModel
+}
+
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -14,7 +21,7 @@ class HBNBCommand(cmd.Cmd):
         """Quits the command intrepreter"""
 
         return True
-    
+
     def help_quit(self):
         """help message for the quit command"""
 
@@ -28,6 +35,41 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """an empty line + ENTER shouldn’t execute anything"""
         pass
+
+    def do_create(self, argument):
+        """Creates a new instance of BaseModel,
+           saves it (to the JSON file) and prints the id
+           If the class name is missing, print a message
+           If the class name doesn’t exist, print a message
+        """
+        if not argument:
+            print("** class name missing ** ")
+        if not argument in class_names:
+            print("** class doesn't exist ** ")
+        else:
+            new_instance = class_names[argument]()
+            new_instance.save()
+            print(new_instance.id)
+
+    def do_show(self, argument):
+        """
+        Prints the string representation of an instance,
+        based on the class name and id
+        """
+        if not argument:
+            print("** class name missing ** ")
+        else:
+            args = argument.split(" ")
+            if args[0] not in class_names:
+                 print("** class doesn't exist ** ")
+            elif len(args) < 2:
+                print("** instance id missing ** ")
+            else:
+                key = args[0] + "." + args[1]
+                
+
+
+
 
 """prevents execution when code is imported"""
 if __name__ == '__main__':
