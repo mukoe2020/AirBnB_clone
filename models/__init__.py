0c3193472e0d9132b2e38554ced97e695915515e
memoe <memoemukoe@gmail.com>
2023-10-11 11:59:52 +0200

    testing

--- models/__init__.py
+++ models/__init__.py
@@ -2,8 +2,5 @@
 from models.engine.file_storage import FileStorage
 """import filestorage to handle data"""
 
-"""Create a unique FileStorage instance"""
 storage = FileStorage()
-
-""" Call the reload() method to load data from the JSON file """
 storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,54 +1,51 @@
 #!/usr/bin/python3
-"""Type module of BaseModel"""
+"""Script that used to create the base class"""
 
-import models
-from uuid import uuid4
+import uuid
 from datetime import datetime
 from models import storage
 
 
-class BaseModel():
-    """
-    Defining parent class for future inheritance"""
+class BaseModel:
 
-
+    """Base class for other classes"""
     def __init__(self, *args, **kwargs):
-        """Type method initialize"""
-        timeformat = "%Y-%m-%dT%H:%M:%S.%f"
-        if len(kwargs) != 0:
-            for key, val in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(val, timeformat))
-                elif key != '__class__':
-                    setattr(self, key, val)
+        """Initialising the id and updated time of instances
+
+        Args:
+            args(string): Unused
+            kwargs(dict): A dictionary of keys and values declared during
+                            instantiation.
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'updated_at' or key == 'created_at':
+                        value = datetime.fromisoformat(value)
+                    setattr(self, key, value)
         else:
-            self.id = str(uuid4())
-            self.created_at = datetime.today()
-            self.updated_at = datetime.today()
-            models.storage.new(self)
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+
+    def __str__(self):
+        """
+          Makes the attribute as strings
+        """
+        return "[{}] ({}) {}".\
+            format(__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """updates the public instance attribute updated_at
-        with the current datetime and saves the instance using storage
+        """
+          Saves changes made to the objects
         """
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """
-        returns a dictionary containing all keys/values of dict
-        of the instance
-        """
-        resulting_dict = self.__dict__.copy()
-        resulting_dict["created_at"] = self.created_at.isoformat()
-        resulting_dict["updated_at"] = self.updated_at.isoformat()
-        resulting_dict["class"] = self.__class__.__name__
-        
-        return resulting_dict
-
-    def __str__(self):
-        """returns informal representation of an instance
-        in the format [<class name>] (<self.id>) <self.to_dict>"""
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.to_dict())
-
+        dict_copy = self.__dict__.copy()
+        dict_copy["__class__"] = self.__class__.__name__
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
+        return dict_copy
