30d41c64d66275d47d238376fa0416b24832fd74
habibaadam <habiepalmer@gmail.com>
2023-10-12 23:14:06 +0000

    10. Console 1.0

--- console.py
+++ console.py
@@ -21,11 +21,7 @@ class_names = {
     'Place': Place,
     'Review': Review
 }
-"""Creates a dictionary mapping class names to class objects """
-    class_names = {
-        'BaseModel': BaseModel,
-        'User': User
-    }
+
 
 class HBNBCommand(cmd.Cmd):
     """Contains the entry point of the command interpreter."""
@@ -51,7 +47,8 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, argument):
         """
-        Creates a new instance of a class, saves it to the JSON file, and prints the ID.
+        Creates a new instance of a class, saves it to the JSON file,
+        and prints the ID.
         If the class name is missing or doesn't exist, it prints a message.
         """
         if not argument:
@@ -65,7 +62,8 @@ class HBNBCommand(cmd.Cmd):
 
     def do_show(self, argument):
         """
-        Prints the string representation of an instance based on the class name and ID.
+        Prints the string representation of an instance
+        based on the class name and ID.
         """
         if not argument:
             print("** class name missing**")
@@ -84,9 +82,11 @@ class HBNBCommand(cmd.Cmd):
                     print("No instance found")
 
     def do_destroy(self, argument):
-        """Deletes an instance based on the class name and ID (saves the change into the JSON file)"""
+        """Deletes an instance based on the class name and ID
+        (saves the change into the JSON file)
+        """
         if not argument:
-          print("** class name missing **")
+            print("** class name missing **")
         else:
             args = argument.split(" ")
             if args[0] not in class_names:
@@ -129,11 +129,11 @@ class HBNBCommand(cmd.Cmd):
             print([str(obj) for obj in objs.values()])
         else:
             args = argument.split(" ")
-            if args[0] not in self.class_names:
+            if args[0] not in class_names:
                 print("** class doesn't exist **")
             else:
                 class_name = args[0]
-                print([str(obj) for obj in objs.values() if obj.__class__.__name__ == class_name])
+                print([str(obj) for obj in objs.values()
                        if obj.__class__.__name__ == class_name])
 
     def do_update(self, argument):
@@ -152,12 +152,12 @@ class HBNBCommand(cmd.Cmd):
                 if len(args) < 3:
                     print("** attribute name missing **")
                 elif len(args) < 4:
-                  print(** value missing **)
+                    print("** value missing **")
                 else:
                     instance = objs[obj_key]
                     setattr(instance, args[2], args[3])
                     instance.save()
-          else:
+            else:
                 print("** no instance found **")
 
 
