b2d977dda43d142f193ffa047ca408e39673cfae
memoe <memoemukoe@gmail.com>
2023-10-13 00:02:00 +0200

    push file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,15 +7,10 @@ from models.user import User
 
 
 class FileStorage:
-    """ Private class attributes """
+    """ this a file that serialize and deserialize object to json file  
+    and from a json file  """
     file_path = "file.json"
     __objects = {}
-
-    class_names = {
-        'BaseModel': BaseModel,
-        'User': User
-    }
-    
     
     def all(self):
         """ Returns the dictionary __objects """
@@ -26,6 +21,26 @@ class FileStorage:
         key = f"{type(obj).__name__}.{obj.id}"
         FileStorage.__objects[key] = obj
 
+
+     def classes(self):
+        """A list of valid classes"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.city import City
+        from models.place import Place
+        from models.amenity import Amenity
+        from models.state import State
+        from models.review import Review
+
+        class_names {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
+
     def save(self):
         """ Serializes __objects to the JSON file (path: __file_path) """
         with open(FileStorage.file_path, "w") as file:
@@ -33,6 +48,45 @@ class FileStorage:
             for key, value in FileStorage.__objects.items():
                 objects_dict[key] = value.to_dict()
             json.dump(objects_dict, file)
+
+
+    def class_attributes(self):
+        """Returns the valid attributes and their types for classname"""
+        class_attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
+        }
+        return class_attributes
 
     def reload(self):
         try:
