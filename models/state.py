48c00178d807e5d1cd3eb09aececcea91d423fc4
habibaadam <habiepalmer@gmail.com>
2023-10-12 13:58:33 +0000

    updates

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module creates the state class class """
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Class for managing state """
+
+    name = ""
---
+++ models/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" the clas user module"""
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """ User class definition """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
